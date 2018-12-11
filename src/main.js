cb642ff28e0dc5a7553f1e1a72e1ba208c1e01fe
TiTu <37405123+ReportMyTeam@users.noreply.github.com>
2018-12-11 10:36:03 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Meetings from './Meetings.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import VueResource from "vue-resource";
 
+Vue.use(VueResource);
+Vue.http.options.root = '/api';
+
 new Vue({
   render: h => h(Meetings),
 }).$mount('#app');

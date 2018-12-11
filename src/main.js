3fdb9e599e82ce190c61dafadaca441be7976faf
TiTu <37405123+ReportMyTeam@users.noreply.github.com>
2018-12-11 10:34:44 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Meetings from './Meetings.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import VueResource from "vue-resource";
 
 new Vue({
   render: h => h(Meetings),
